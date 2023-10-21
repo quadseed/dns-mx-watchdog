93546c966ebfb365d7b0151da9e1749636aceb31
quadseed <contact@quadseed.net>
2023-10-21 14:52:48 +0900

    Add onReady lookup

--- main.go
+++ main.go
@@ -18,27 +18,18 @@ func main() {
 
 	client := slack.New(token)
 
-	ticker := time.NewTicker(time.Millisecond * 60 * 60 * 1000)
+	ticker := time.NewTicker(time.Millisecond * 1000 * 60 * 60)
 	defer ticker.Stop()
 
 	log.Printf("DNS Watchdog task has been started")
 
 	count := 0
+	execLookup(*client, channelId, dnsServer, domain, &count)
 	for {
 		select {
 		case <-ticker.C:
 			log.Printf("count=%d\n", count)
-			ok := lookupMXRecords(dnsServer, domain)
-			if ok {
-				count++
-				if count > 24 {
-					count = 0
-					SendDailyNotification(*client, channelId, dnsServer, domain)
-				}
-			} else {
-				count = 0
-				SendHourlyNotification(*client, channelId, dnsServer, domain)
-			}
+			execLookup(*client, channelId, dnsServer, domain, &count)
 		}
 	}
 }
@@ -60,4 +51,18 @@ func lookupMXRecords(dnsServer string, domain string) bool {
 		}
 	}
 	return true
+}
+
+func execLookup(client slack.Client, channelId string, dnsServer string, domain string, count *int) {
+	ok := lookupMXRecords(dnsServer, domain)
+	if ok {
+		*count++
+		if *count > 24 {
+			*count = 0
+			SendDailyNotification(client, channelId, dnsServer, domain)
+		}
+	} else {
+		*count = 0
+		SendHourlyNotification(client, channelId, dnsServer, domain)
+	}
 }
